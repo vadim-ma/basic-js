7abd99859c7354d7de62e36afc5a249703ece2dd
vadim-ma <vadimmitin@mail.ru>
2023-12-21 07:35:50 +0300

    feat: implement Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const ret = [];
+  let discardNext = false;
+  let doubleNext = false;
+  let prevNotExist = true;
+
+  arr.forEach(v => {
+    switch (v) {
+      case '--discard-next':
+        discardNext = true;
+        doubleNext = false;
+        break;
+
+      case '--double-next':
+        discardNext = false;
+        doubleNext = true;
+        break;
+    
+      case '--discard-prev':
+        discardNext = false;
+        doubleNext = false;
+        if (!prevNotExist){
+          if (ret.length > 0) {
+            ret.pop();
+            prevNotExist = true;
+          }
+        }
+        break;
+
+      case '--double-prev':
+        if (!prevNotExist) {
+          if (ret.length > 0) {
+            ret.push(ret[ret.length - 1]);
+            prevNotExist = false;
+          }
+        }
+        discardNext = false;
+        doubleNext = false;
+        break;
+  
+      default:
+        if (discardNext) {
+          prevNotExist = true;
+          discardNext = false;
+          doubleNext = false;
+          break;
+        }
+        ret.push(v);
+        prevNotExist = false;
+        if (doubleNext) {
+          ret.push(v);
+        }
+
+        discardNext = false;
+        doubleNext = false;
+        break;
+    }
+  });
+  return ret;
 }
 
 module.exports = {
